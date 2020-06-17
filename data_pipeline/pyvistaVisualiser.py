d253d95252ccc625b131c7d0814380d136275561
rishyak <rishyak@gmail.com>
2020-06-17 20:22:46 +0530

    The new visualiser.

--- data_pipeline/pyvistaVisualiser.py
+++ data_pipeline/pyvistaVisualiser.py
@@ -1,19 +1,13 @@
 import numpy as np
-import sys
 import pyvista as pv
 
 data_matrix = np.load('maskdatamaskedimages_test.npy')
 
-# np.savetxt("array.txt", data_matrix[1])
-
-# np.set_printoptions(threshold=sys.maxsize)
-
-# print(data_matrix[1])
-
-opacity = [0, 0, 0, 4, 8, 0, 0] # 7
+opacity = [0, 0, 0, 4, 8, 0, 0] 
 
 data = pv.wrap(data_matrix)
 pv.set_plot_theme("night")
-data.plot(volume=True, cmap="cool", opacity = opacity, shade = False)
 
-# "sigmoid_6"
+data.save("lung.vtk")
+
+data.plot(volume=True, cmap="cool", opacity = opacity, shade = False)
