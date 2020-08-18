aa481a36bbf81ffe4399e2bcd842d926457044ff
g1776 <gregoryg323@gmail.com>
2020-08-18 15:16:14 -0400

    Beginning of UI overhaul

--- Back-end/vessel_app/auth/routes.py
+++ Back-end/vessel_app/auth/routes.py
@@ -19,13 +19,13 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
+            login_user(user)
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('auth.account'))
             return redirect(url_for('main.home'))
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
+    return render_template('login2.html', title='Login', form=form)
 
 @bp.route("/register", methods=['GET', 'POST'])
 def register():
