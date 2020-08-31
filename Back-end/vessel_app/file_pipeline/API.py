eb827752e17450e6630206b4412608f8eafb413e
Gresliebear <lesliemwubbel@gmail.com>
2020-08-31 00:46:23 -0400

    Getting start update

--- Back-end/vessel_app/file_pipeline/API.py
+++ Back-end/vessel_app/file_pipeline/API.py
@@ -3,3 +3,10 @@
 ##Epic's APi 
 ## 
 
+
+
+#### API query for HL7 
+## ingestion framework 
+message = 'MSH|^~\&|GHH LAB|ELAB-3|GHH OE|BLDG4|200202150930||ORU^R01|CNTRL-3456|P|2.4\r'
+import hl7 
+test = hl7.parse(message)
