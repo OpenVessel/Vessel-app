3cc7db8201e3315b4f5705700edced9271e49204
Shubhang <shubhangsharma24@gmail.com>
2021-04-28 15:37:03 -0400

    Clean Up Master
    --Archived in Branch: "Master-Archive"

--- Back-end/.flaskenv
+++
@@ -1,4 +0,0 @@
-FLASK_DEBUG=0
-FLASK_APP=wsgi.py
-FLASK_ENV=production
-SECRET_KEY='insert secret_key'
--- Back-end/wsgi.py
+++
@@ -1,6 +0,0 @@
-from vessel_app import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run(ssl_context='adhoc')
